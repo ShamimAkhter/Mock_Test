fa33bc95d123fb044238451057b6ea4317490767
Shamim Akhter <shamimakhter25@outlook.com>
2023-02-02 01:07:07 +0530

    Created a separate staging environment which will contain configurations for local IIS deployment

---
+++ Readme.md
@@ -0,0 +1,2 @@
+`ng build --configuration staging` - for local IIS deployment 
+`ng build --configuration production` - for live IIS deployment
--- examination-ng/src/environments/environment.staging.ts
+++ examination-ng/src/environments/environment.staging.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://questpondinterviews.com'
+  baseUrl: 'https://mocktest.com'
 };
